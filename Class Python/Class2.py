5856ac272d5c043af64dee02e31a165ebaffce52
blackwhite0625 <f0989724842@gmail.com>
2024-11-20 11:52:57 +0800

    add

--- Class Python/Class2.py
+++ Class Python/Class2.py
@@ -14,3 +14,54 @@ def ran():
     print(i)
     if i > 12:
         print("sum12")
+
+#剪刀石頭布
+def rock():
+    while True:
+        #剪刀1 石頭2 布3
+        randoms = ["1","2","3"]
+        
+        rockrandom = random.choice(randoms)
+  
+        a = input("猜拳:")
+        if a == rockrandom:
+            print("平手")
+        elif (a == "1" and rockrandom == "3")or(a == "2" and rockrandom == "1")or(a == "3" and rockrandom == "2"):
+            print("你贏了 對手出的是:", rockrandom)
+        else:
+            print("你輸了廢物 對手出的是:",rockrandom)
+        
+        res =  input("是否要繼續:(輸入是否)")
+        if res != "是":
+            break
+        else:
+            continue
+
+
+def textf():
+    a = "aiopopu"
+    b= a.find("io")
+    print(b)
+    
+    c = "ieoskds"
+    d = c.upper()
+    print(d)
+    
+    for e in "sfgfdgf":
+        print(e)
+    for f in range(2,5):
+        print(f)
+    
+    # 九九乘法表
+    for i in range(1, 10):  
+        for j in range(1, i + 1):  
+            print(f"{j} x {i} = {i * j:2}", end="  ") 
+        print()  
+
+    # **行數
+    rows = int(input("請輸入需要的行數: "))
+    for i in range(1, rows + 1): 
+        print("*" * i)  
+
+        
+textf()
